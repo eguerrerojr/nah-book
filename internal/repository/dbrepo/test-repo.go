f5dd7004ba2a50a7c6a4a6f1f5460fa82792fff3
junedgard <74942638+junedgard@users.noreply.github.com>
2021-09-30 14:24:11 +0800

    Showing one reservation

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -125,3 +125,10 @@ func (m *testDBRepo) AllNewReservations() ([]models.Reservation, error) {
 
 	return reservations, nil
 }
+
+//GetReservationByID
+func (m *testDBRepo) GetReservationByID(id int) (models.Reservation, error) {
+	var res models.Reservation
+
+	return res, nil
+}
